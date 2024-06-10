5c4dab75480877037b4aaaa839948410fa33aee8
Toshiaki Maki <makingx@gmail.com>
2024-06-10 14:53:51 +0900

    Expand NullAway:AnnotatedPackages

--- src/main/java/com/example/opinionated/hello/HelloController.java
+++ src/main/java/com/example/opinionated/hello/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.opinionated.hello;
 
+import java.util.Objects;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,7 +19,9 @@ public class HelloController {
 
 	@GetMapping(path = "/api/hello")
 	public JsonNode hello(@RequestParam(defaultValue = "Hello World!") String message) {
-		return this.restClient.get().uri("/get?text={message}", message).retrieve().body(JsonNode.class).get("args");
+		return Objects
+			.requireNonNull(this.restClient.get().uri("/get?text={message}", message).retrieve().body(JsonNode.class))
+			.get("args");
 	}
 
 }
